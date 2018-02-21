e7c1378ae8ca594d6be4eb810c84705fbff44bb0
Ian Johnson <ian.johnson@canonical.com>
2018-02-21 17:36:02 -0600

    Adding snapcore function to test as well
    We now have an additional property in the json table that tests using the FileDigest method from snapcore.
    Also deleted duplicate sha512 from the list of hashers.

--- cmd/crypto-tester/main.go
+++ cmd/crypto-tester/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
@@ -14,30 +15,45 @@ import (
 	"strings"
 	"time"
 
+	osutil "github.com/snapcore/snapd/osutil"
+
 	md4 "golang.org/x/crypto/md4"
 	ripemd160 "golang.org/x/crypto/ripemd160"
 	sha3 "golang.org/x/crypto/sha3"
 )
 
-func timeHash(hasher hash.Hash, bytes []byte) ([]byte, time.Duration) {
+func timeBytesHash(hasher hash.Hash, bytes []byte) ([]byte, time.Duration) {
 	// Get start time
 	start := time.Now()
 
-	// Compute the time
+	// Compute the hash of the bytes
 	hashRes := hasher.Sum(bytes)
 
 	// Return the hash result and the time since the start of the function
 	return hashRes, time.Since(start)
 }
 
+func timeFileHash(hasher crypto.Hash, file string) ([]byte, time.Duration) {
+	// Get start time
+	start := time.Now()
+
+	// Compute the hash of the file
+	hashRes, _, _ := osutil.FileDigest(file, hasher)
+
+	// Return the hash result and the time since the start of the function
+	return hashRes, time.Since(start)
+}
+
 type HasherType struct {
-	Name   string
-	Hasher hash.Hash
+	Name     string
+	Hasher   hash.Hash
+	HashType crypto.Hash
 }
 
 type HashResult struct {
-	Algorithm string `json:"alg"`
-	TimeNs    int64  `json:"time_ns"`
+	Algorithm   string `json:"alg"`
+	BytesHashNs int64  `json:"bytes_ns"`
+	FileHashNs  int64  `json:"file_ns"`
 }
 
 func main() {
@@ -62,11 +78,13 @@ func main() {
 	// For each hasher run the hash and print off how long it took
 	for index, hasher := range hasherTable {
 		// Compute the hash for each one
-		_, timeElapsed := timeHash(hasher.Hasher, bytes)
+		_, timeElapsedBytes := timeBytesHash(hasher.Hasher, bytes)
+		_, timeElapsedFile := timeFileHash(hasher.HashType, *fileStr)
 
 		results[index] = HashResult{
-			Algorithm: hasher.Name,
-			TimeNs:    int64(timeElapsed / time.Nanosecond),
+			Algorithm:   hasher.Name,
+			BytesHashNs: int64(timeElapsedBytes / time.Nanosecond),
+			FileHashNs:  int64(timeElapsedFile / time.Nanosecond),
 		}
 	}
 
@@ -80,21 +98,20 @@ func main() {
 
 func initializeHasherTable() []HasherType {
 	return []HasherType{
-		{"md4", md4.New()},
-		{"md5", md5.New()},
-		{"sha1", sha1.New()},
-		{"sha256", sha256.New()},
-		{"sha256_224", sha256.New224()},
-		{"sha512", sha512.New()},
-		{"sha512_224", sha512.New512_224()},
-		{"sha512_256", sha512.New512_256()},
-		{"sha512", sha512.New()},
-		{"sha384", sha512.New384()},
-		{"ripemd160", ripemd160.New()},
-		{"sha3_224", sha3.New224()},
-		{"sha3_256", sha3.New256()},
-		{"sha3_384", sha3.New384()},
-		{"sha3_512", sha3.New512()},
+		{"md4", md4.New(), crypto.MD4},
+		{"md5", md5.New(), crypto.MD5},
+		{"sha1", sha1.New(), crypto.SHA1},
+		{"sha256", sha256.New(), crypto.SHA256},
+		{"sha256_224", sha256.New224(), crypto.SHA224},
+		{"sha512", sha512.New(), crypto.SHA512},
+		{"sha512_224", sha512.New512_224(), crypto.SHA512_224},
+		{"sha512_256", sha512.New512_256(), crypto.SHA512_256},
+		{"sha384", sha512.New384(), crypto.SHA384},
+		{"ripemd160", ripemd160.New(), crypto.RIPEMD160},
+		{"sha3_224", sha3.New224(), crypto.SHA3_224},
+		{"sha3_256", sha3.New256(), crypto.SHA3_256},
+		{"sha3_384", sha3.New384(), crypto.SHA3_384},
+		{"sha3_512", sha3.New512(), crypto.SHA3_512},
 		// Disabled for now...
 		// {"blake2s128", blake2s.New128(nil)},
 		// {"blake2s256", blake2s.New256(nil)},
